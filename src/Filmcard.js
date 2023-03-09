601e6f3015713e7151e6a5306b37b37eac4357dd
GusWallin <61548296+GusWallin@users.noreply.github.com>
2023-03-09 02:18:35 +0100

    3 files modified

--- src/Filmcard.js
+++ src/Filmcard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function Filmcard(props){
-
-return(
-<p>
-  filmcard
-</p>
-
-
-)
-
-
-  
+export default function Filmcard(props) {
+  console.log(props);
+  return (
+    <div className="filmcard">
+      <p className="filmcard--title">Title of Movie: {props.title}</p>
+      <p className="filmcard--realeasedate">Releasedate: {props.release}
+      </p>
+      <p className="filmcard--realeasedate">Director: {props.director}
+      </p>
+      <p className="filmcard--crawl">Main Crawl of movie: {props.crawl}</p>
+    </div>
+  );
 }
--- src/Main.js
+++ src/Main.js
@@ -2,16 +2,41 @@ import React from 'react';
 import Filmcard from './Filmcard';
 
 export default function Main(props) {
-  let title = props.films.map((film) => film.title);
-  console.log(title);
+  const filmElements = props.films.map((film) => {
+    return (
+      <Filmcard
+        title={film.title}
+        crawl={film.opening_crawl}
+        director={film.director}
+        release={film.release_date}
+      />
+    );
+  });
+
+/*   let title = props.films.map((film) => {
+    return film.title;
+  });
+  let crawl = props.films.map((film) => {
+    return film.opening_crawl;
+  });
+  let releaseDate = props.films.map((film) => {
+    return film.release_date;
+  }); */
+  //let title = props.films.map((film)) => film.title);
+  // console.log(title[0]);
+
   return (
     <div className="Main--container">
       <h1> Search for movie </h1>
       <input className="searchBox" type="text" placeholder="Text"></input>
       <button type="submit">SEARCH</button>
       <section>
-        <Filmcard />
-        <p>{title}</p>
+        {filmElements}
+        {/* <Filmcard
+          title={title[0]}
+          crawl={crawl[0]}
+          releaseDate={releaseDate[0]}
+        /> */}
       </section>
     </div>
   );
